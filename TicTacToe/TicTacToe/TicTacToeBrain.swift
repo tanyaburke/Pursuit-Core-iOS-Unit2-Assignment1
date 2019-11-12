33ed172809cada986a7abc877f41f6971919035e
tanyaburke <tanyaburke@pursuit.org>
2019-11-12 15:28:19 -0500

    Working version

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,8 +6,138 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+//var turnCount = 0
+@available(iOS 13.0, *)
 
 class TicTacToeBrain {
-  
+    var turnCount = 0
+    var pressedButtonMatrix = [["","",""],
+                               ["","",""],
+                               ["","",""]]
+    var winner = ""
+    var  xcount = 0
+    var ocount = 0
+    var gameOver = false
+    
+    func playerTurnStart(gameButton: GameButton) -> (String){
+        if turnCount % 2 == 0 {
+            
+            pressedButtonMatrix[gameButton.row][gameButton.col] = "X"
+            gameButton.isEnabled = false
+            turnCount += 1; gameButton.setBackgroundImage(UIImage(systemName:"xmark"), for: .normal)
+            print(pressedButtonMatrix)
+            
+            //check winner here
+            return "Player Two Turn"
+            
+        } else {
+            
+            pressedButtonMatrix[gameButton.row][gameButton.col] = "O"
+            turnCount += 1
+            gameButton.setBackgroundImage(UIImage(systemName:"circle"), for: .normal)
+           
+            gameButton.isEnabled = false
+                if gameOver == false{
+                
+                gameButton.isEnabled = false
+            }
+        
+            gameButton.imageView?.alpha = 1
+            print(pressedButtonMatrix)
+            //check winner
+            return "Player One Turn"
+        }
+    }
+   
+    func disableButtons(gameButton: [GameButton]) {
+          for button in gameButton {
+     
+           button.isEnabled = false
+          
+      }
+    }
+
+    
+    func winnerX() -> String {
+        
+        for i in 1..<pressedButtonMatrix.count{
+            for j in 0..<pressedButtonMatrix[i].count{
+                if pressedButtonMatrix[i][j] == "X" {
+                    xcount += 1
+                }
+                if xcount == 3 {
+                    
+                    winner = "Player One Won!!"
+                    gameOver = true
+                }
+            }
+            xcount = 0
+        }
+        return winner
+    }
+    
+    
+    
+    func winnerO() -> String {
+//        var winner = "no winner"
+        for i in 0..<pressedButtonMatrix.count {
+            for j in 0..<pressedButtonMatrix[i].count{
+                if pressedButtonMatrix[i][j] == "O" {
+                    ocount += 1
+                }
+                if ocount == 3 {
+                    winner = "Player Two Won!!"
+                    gameOver = true
+                }
+            }
+            ocount = 0
+        }
+        return winner
+    }
+    
+    
+    
+    
+    func winnerOcol() -> String {
+    //        var winner = "no winner"
+            for i in 0..<pressedButtonMatrix.count {
+                for j in 0..<pressedButtonMatrix[i].count{
+                    if pressedButtonMatrix[j][i] == "O" {
+                        ocount += 1
+                    }
+                    if ocount == 3 {
+                        winner = "Player Two Won!!"
+                        gameOver = true
+                    }
+                }
+                ocount = 0
+            }
+            return winner
+        }
+    
+    
+    
+
+    func winnerXcol() -> String {
+    //        var winner = "no winner"
+            for i in 0..<pressedButtonMatrix.count {
+                for j in 0..<pressedButtonMatrix[i].count{
+                    if pressedButtonMatrix[j][i] == "X" {
+                        xcount += 1
+                    }
+                    if xcount == 3 {
+                        winner = "Player One Won!!"
+                        gameOver = true
+                    }
+                }
+                xcount = 0
+            }
+            return winner
+        }
+    
+
+   
 }
+
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -7,22 +7,44 @@
 //
 
 import UIKit
+@available(iOS 13.0, *)
 
 class ViewController: UIViewController {
     @IBOutlet var gameButtons: [GameButton]!
-   
+    @IBOutlet weak var playerTurn: UILabel!
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    var gameStart = TicTacToeBrain()
+    
+    
+    
     //outlets don't go below the override
-    
-    
+//    var turnCount = 0
+//    var buttonPressedArray = [GameButton]
   override func viewDidLoad() {
     super.viewDidLoad()//can add color here so it would come up when it loads
     // Do any additional setup after loading the view, typically from a nib.
   }
-//Actions, method, initializer/ prperty
-    @IBAction func gameButtonPressed(_ gameButton: GameButton) { //refactored by right clicking, refactor, rename or will have errors
-        //array of array, keeping track of the buttons pressed
-        print("row \(gameButton.row) at column \(gameButton.col) was selected")
-    }
     
-}
+    
+    
+//Actions, method, initializer/ prperty
+    @IBAction func gameButtonPressed(_ gameButton: GameButton) { //refactored by right clicking, refactor,
+        
+        
+        
+        print("row \(gameButton.row) at column \(gameButton.col) was selected")
+        playerTurn.text = gameStart.playerTurnStart(gameButton: gameButton)
+        
+        winnerLabel.text =  "\(gameStart.winnerX())"
+        
+        winnerLabel.text = "\(gameStart.winnerO())"
+        winnerLabel.text = "\(gameStart.winnerOcol())"
+        winnerLabel.text = "\(gameStart.winnerXcol())"
 
+        
+        
+        
+        
+}
+}
